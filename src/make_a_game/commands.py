dcce69458d79bc1ab124300ceba9d4e3f0743cd7
Jeffrey Baumes <jeff.baumes@kitware.com>
2017-11-12 11:57:36 -0500

    Add missing file

---
+++ src/make_a_game/commands.py
@@ -0,0 +1,19 @@
+from twisted.protocols import amp
+
+
+class GetChunk(amp.Command):
+    arguments = [(b'x', amp.Integer()),
+                 (b'y', amp.Integer())]
+    response = [(b'chunk', amp.Unicode())]
+
+
+class UpdateChunk(amp.Command):
+    arguments = [(b'chunk', amp.Unicode())]
+    response = [(b'result', amp.Boolean())]
+
+
+class UpdateMaterial(amp.Command):
+    arguments = [(b'x', amp.Integer()),
+                 (b'y', amp.Integer()),
+                 (b'material', amp.Integer())]
+    response = [(b'result', amp.Boolean())]
